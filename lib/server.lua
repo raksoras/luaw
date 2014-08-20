08b8186adc15c3b4228451cbc806a9a393d81871
raksoras <susheel.aroskar@gmail.com>
2014-08-20 23:22:19 +0000

    testing

--- lib/server.lua
+++ lib/server.lua
@@ -3,8 +3,60 @@ local logging = require("luaw_logging")
 local server = require("luaw_server")
 local webapp = require("luaw_webapp")
 
+local function proxyHTTPStreaming(conn)
+    while true do
+        local req = luaw_lib.newServerHttpRequest(conn)    
+        local body = req:readFull()
+        local url = req:getParsedURL()
+
+        if ((url)and(url.path)) then
+            local proxyReq = luaw_lib.newClientHttpRequest()
+            proxyReq.hostName = "hacksubscriber.us-east-1.dyntest.netflix.net"
+            proxyReq.method = 'GET'
+            proxyReq.url = url.path
+            proxyReq.port = 7001
+            proxyReq.headers = { Host = "hacksubscriber.us-east-1.dyntest.netflix.net" }
+
+            local proxyResp = proxyReq:connect()
+            proxyReq:flush()
+
+            local headersDone, mesgDone, body = false, false, nil
+            while not headersDone do
+                headersDone, mesgDone, body = proxyResp:readStreaming()
+            end
+            
+            local resp = luaw_lib.newServerHttpResponse(conn)
+            resp:setStatus(proxyResp.status)
+            local headers = proxyResp.headers
+            for k,v in pairs(headers) do
+                if (k ~= 'Transfer-Encoding') then
+                    resp:addHeader(k,v)
+                end
+            end
+            
+            resp:startStreaming()
+            
+            while true do
+                if body then 
+                    resp:appendBody(body) 
+                end
+                if mesgDone then break end
+                headersDone,  mesgDone, body = proxyResp:readStreaming()
+            end
+
+            resp:flush()
+            proxyResp:close()
+            
+            if (req:shouldCloseConnection() or resp:shouldCloseConnection()) then
+                resp:close()
+                break
+            end
+        end
+    end
+end
+
 local config = server.loadConfiguration(...)
-config.request_handler = webapp.serviceHTTP
+config.request_handler = proxyHTTPStreaming
 
 print("Starting server...") 
 
