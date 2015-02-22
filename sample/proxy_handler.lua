127ea500456521d2d12d99b9e52b47f853480109
raksoras <susheel.aroskar@gmail.com>
2015-02-21 17:30:37 -0800

    fix sample code

--- sample/proxy_handler.lua
+++ sample/proxy_handler.lua
@@ -1,3 +1,12 @@
+--[[
+    Luaw allows you to replace it's default MVC/REST request handler with your own custom HTTP 
+request handler implementation. To override the default HTTP request handler just set Luaw object's 
+request_handler property to your custom Lua function. This function is passed in a low level connection 
+object for each incoming request instead of the normal request and response objects passed to REST handler. 
+The function is called on its own separate Luaw coroutine for each HTTP request so you don't have to worry 
+about multithreaded access to shared state inside the function.
+]]
+
 Luaw.request_handler =  function(conn)
     conn:startReading()
 
@@ -18,30 +27,30 @@ Luaw.request_handler =  function(conn)
         local beURL = reqHeaders['backend-url']
 
         if (beHost and beURL) then
-            local backendReq = Luaw.newClientHttpRequest()
-            backendReq.hostName = beHost
-            backendReq.url = beURL
-            backendReq.method = 'GET'
-            backendReq.headers = { Host = beHost }
+           local backendReq = Luaw.newClientHttpRequest()
+           backendReq.hostName = beHost
+           backendReq.url = beURL
+           backendReq.method = 'GET'
+           backendReq.headers = { Host = beHost }
 
-            local status, backendResp = pcall(backendReq.execute, backendReq)
-            if (status) then
-                resp:setStatus(backendResp:getStatus())
-                resp:appendBody(backendResp:getBody())
-                local beHeaders = backendResp.headers
-                for k,v in pairs(beHeaders) do
-                    if ((k ~= 'Transfer-Encoding')and(k ~= 'Content-Length')) then
-                        resp:addHeader(k,v)
-                    end
-                end
-                backendResp:close()
+           local status, backendResp = pcall(backendReq.execute, backendReq)
+           if (status) then
+               resp:setStatus(backendResp:getStatus())
+               resp:appendBody(backendResp:getBody())
+               local beHeaders = backendResp.headers
+               for k,v in pairs(beHeaders) do
+                   if ((k ~= 'Transfer-Encoding')and(k ~= 'Content-Length')) then
+                       resp:addHeader(k,v)
+                   end
+               end
+               backendResp:close()
             else
-                resp:setStatus(500)
-                resp:appendBody("connection to backend server failed")
-            end
+               resp:setStatus(500)
+               resp:appendBody("connection to backend server failed")
+           end
         else
             resp:setStatus(400)
-            resp:appendBody("Request must contain headers backend-host and backend-url")
+            resp:appendBody("Request must contain headers backend-host and backend-url\n")
         end
 
         local status, mesg = pcall(resp.flush, resp)
--- sample/webapps/myapp/web.lua
+++ sample/webapps/myapp/web.lua
@@ -2,24 +2,3 @@ luaw_webapp = {
     resourcePattern = "handler%-.*%.lua",
     viewPattern = "view%-.*%.lua",
 }
-
-Luaw.logging.file {
-    name = "root",
-    level = Luaw.logging.ERROR,
-}
-
-Luaw.logging.file {
-    name = "com.luaw",
-    level = Luaw.logging.INFO,
-}
-
-Luaw.logging.syslog {
-    name = "com.luaw",
-    level = Luaw.logging.WARNING,
-}
-
---Luaw.logging.category {
---    name = 'com',
---    level = Luaw.logging.WARNING,
---    target = Luaw.logging.FILE
---}
